e46beb767d025a31973de4c6937a7fd78186f21b
jamal <fdadf>
2021-07-25 11:29:45 +0430

    Improve: webpack.config.js internal structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,72 +4,71 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
-module.exports = {
-  mode: 'development',
-  entry: "/src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve("dist"),
-    publicPath: "/",
-  },
-  devServer: {
+const basics = {
+    mode: 'development',
+    entry: "/src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve("dist"),
+        publicPath: "/",
+    },
+}
+
+const devServer = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 2006,
     hot: true,
-    hotOnly: true,
     inline: true,
     liveReload: true,
+}
 
-
-  },
-  watchOptions: {
-    poll: true,
-    ignored: '/node_modules/',
-  },
-
-  // -----Aliases for path-----------------------------
-  resolve: {
+const resolve = {
     alias: {
-      Utils: path.resolve(__dirname, 'src/utils/'),
+        Utils: path.resolve(__dirname, 'src/utils/'),
     },
-  },
-  // --------------------------------------------------
-  module: {
-    rules: [{
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
+}
+
+const rules = [{
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: "babel-loader"
+},
+    {
         test: /\.html$/,
         use: "html-loader"
-      },
-      /*Choose only one of the following two: if you're using 
-      plain CSS, use the first one, and if you're using a
-      preprocessor, in this case SASS, use the second one*/
-      {
+    },
+    /*Choose only one of the following two: if you're using
+    plain CSS, use the first one, and if you're using a
+    preprocessor, in this case SASS, use the second one*/
+    {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
+    },
+    {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+            "style-loader",
+            "css-loader",
+            "sass-loader"
         ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-    }),
-    new HTMLWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new webpack.HotModuleReplacementPlugin({
-    })
+    },
+]
 
-  ]
+const plugins = [
+    new MiniCssExtractPlugin({}),
+    new HTMLWebpackPlugin({
+        template: "./public/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin({})
+]
+
+module.exports = {
+   ... basics,
+    devServer,
+    resolve,
+    module: {
+        rules,
+    },
+    plugins
 }
