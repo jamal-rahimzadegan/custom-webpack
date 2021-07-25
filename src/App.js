be29e2c8d5fa6e713a4942c0ba86bb94af615682
jamal <fdadf>
2021-07-25 11:32:30 +0430

    Feat: added prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {plusAll} from "Utils"
+import { plusAll } from "Utils";
 
-export default function App(){
-     return (
-          <div>
-            <p>App page</p>
-            <button onClick={()=>plusAll(71,0)}>add all</button>
-          </div>
-     )
+export default function App() {
+  return (
+    <div>
+      <p>App page</p>
+      <button onClick={() => plusAll(71, 0)}>add all</button>
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,70 @@
-const path = require("path");
 const webpack = require("webpack");
+const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
 const basics = {
-    mode: 'development',
-    entry: "/src/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve("dist"),
-        publicPath: "/",
-    },
-}
+  mode: "development",
+  entry: "/src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("build"),
+    publicPath: "/",
+  },
+};
 
 const devServer = {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 2006,
-    hot: true,
-    inline: true,
-    liveReload: true,
-}
+  contentBase: path.join(__dirname, "build"),
+  compress: true,
+  port: 2006,
+  hot: true,
+  inline: true,
+  liveReload: true,
+};
 
 const resolve = {
-    alias: {
-        Utils: path.resolve(__dirname, 'src/utils/'),
-    },
-}
+  alias: {
+    Utils: path.resolve(__dirname, "src/utils/"),
+  },
+};
 
-const rules = [{
+const rules = [
+  {
     test: /\.(js|jsx|ts|tsx)$/,
     exclude: /node_modules/,
-    use: "babel-loader"
-},
-    {
-        test: /\.html$/,
-        use: "html-loader"
-    },
-    /*Choose only one of the following two: if you're using
+    use: "babel-loader",
+  },
+  {
+    test: /\.html$/,
+    use: "html-loader",
+  },
+  /*Choose only one of the following two: if you're using
     plain CSS, use the first one, and if you're using a
     preprocessor, in this case SASS, use the second one*/
-    {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-    },
-    {
-        test: /\.scss$/,
-        use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader"
-        ],
-    },
-]
+  {
+    test: /\.css$/,
+    use: [MiniCssExtractPlugin.loader, "css-loader"],
+  },
+  {
+    test: /\.scss$/,
+    use: ["style-loader", "css-loader", "sass-loader"],
+  },
+];
 
 const plugins = [
-    new MiniCssExtractPlugin({}),
-    new HTMLWebpackPlugin({
-        template: "./public/index.html",
-    }),
-    new webpack.HotModuleReplacementPlugin({})
-]
+  new MiniCssExtractPlugin({}),
+  new HTMLWebpackPlugin({
+    template: "./public/index.html",
+  }),
+  new webpack.HotModuleReplacementPlugin({}),
+];
 
 module.exports = {
-   ... basics,
-    devServer,
-    resolve,
-    module: {
-        rules,
-    },
-    plugins
-}
+  ...basics,
+  devServer,
+  resolve,
+  module: {
+    rules,
+  },
+  plugins,
+};
