b1f65202ebb5e01b38ca072c196256a45b3fa715
jamal <fdadf>
2021-10-25 09:45:30 +0330

    Feat: added some express server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => res.send("some dommy content"));
+
+app.listen(port, () => console.log(`--- app is running on  port----> `, port));
