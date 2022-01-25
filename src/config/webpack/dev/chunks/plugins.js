84321f0216b180fdd3ff9b88dc272a22ac6fc8ed
jamal <fdadf>
2022-01-26 01:01:13 +0330

    Feat: added offline support to workbox

--- src/config/webpack/dev/chunks/plugins.js
+++ src/config/webpack/dev/chunks/plugins.js
@@ -4,6 +4,8 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = [
   new HtmlWebpackPlugin({ template: "./public/index.html" }),
@@ -22,5 +24,17 @@ module.exports = [
     // and not allow any straggling "old" SWs to hang around
     clientsClaim: true,
     skipWaiting: true,
+  }),
+
+  new CopyPlugin({
+    patterns: [
+      { from: "./favicon.ico", to: "" },
+      { from: "./manifest.json", to: "" },
+      { from: "./logo512.png", to: "" },
+    ],
+  }),
+  new WorkboxWebpackPlugin.InjectManifest({
+    swSrc: "./src/src-sw.js",
+    swDest: "sw.js",
   }),
 ];
