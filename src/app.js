75cb93b21a8ee349762f1a60fd2ab5b67406f9da
Jamal Rahimzadegan <jamalr@amdocs.com>
2022-11-25 01:09:23 +0300

    Chore: add .env with its config

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./app.css";
 export default function App() {
   const allResult = sumAll(5, 15);
   console.log(`--- result of all ----> `, allResult);
+  console.log(`--- env ----> `, process.env.API_BASE_URL);
 
   const root = document.getElementById("root");
   root.innerHTML = '<p class="app-style">Hello Webpack</p>';
--- src/webpack/modules/plugins.js
+++ src/webpack/modules/plugins.js
@@ -2,16 +2,22 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const dotenv = require("dotenv");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { REGEX } = require("../common/constants");
 
-const generateBasePlugins = (env) => [
-  new webpack.DefinePlugin({
-    "process.env.NODE_ENV": JSON.stringify(env),
-  }),
-  new HtmlWebpackPlugin({ template: "./public/index.html" }),
-];
+const generateBasePlugins = (currentEnv) => {
+  dotenv.config();
+
+  return [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+      "process.env.NODE_ENV": JSON.stringify(currentEnv),
+    }),
+  ];
+};
 
 const prodConfig = [
   ...generateBasePlugins("production"),
